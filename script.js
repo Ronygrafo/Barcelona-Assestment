8acbbbfc75e2c27d0f090485882356ca23d41318
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2023-02-11 10:16:41 -0500

    web components done!. aimation pending

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //alert("hey");
+
+
+
+// =============  Med Buttom Animation ================ //
+
+const poblacionBtn = document.getElementById("poblacion");
+
+//console.log(poblacionBtn.childNodes[1].childNodes[3]);
+
+
+poblacionBtn.addEventListener('click', () => {
+    poblacionBtn.childNodes[3].classList.toggle('ActiveIcon');
+    poblacionBtn.childNodes[1].childNodes[3].classList.toggle("Hidden");
+
+    //console.log(poblacionBtn.childNodes)
+
+  })
+
+/*   const btn = document.querySelector('.btn_animated')
+btn.addEventListener('click', () => {
+  btn.classList.remove('animate')
+  setTimeout(() => btn.classList.add('animate'), 100)
+}) */
